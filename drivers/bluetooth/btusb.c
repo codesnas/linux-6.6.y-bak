b0fdfbe13222ddd8a18f3716d0ab7b1e0e2a2ee1
codesnas <67037511+codesnas@users.noreply.github.com>
2024-01-10 18:56:37 -0800

    Bluetooth: btusb: Add device ID 0e8d:0616 for MT7922(RD616)

--- drivers/bluetooth/btusb.c
+++ drivers/bluetooth/btusb.c
@@ -648,6 +648,9 @@ static const struct usb_device_id quirks_table[] = {
 	{ USB_DEVICE(0x04ca, 0x3804), .driver_info = BTUSB_MEDIATEK |
 						     BTUSB_WIDEBAND_SPEECH |
 						     BTUSB_VALID_LE_STATES },
+	{ USB_DEVICE(0x0e8d, 0x0616), .driver_info = BTUSB_MEDIATEK |
+						     BTUSB_WIDEBAND_SPEECH |
+						     BTUSB_VALID_LE_STATES },
 
 	/* Additional Realtek 8723AE Bluetooth devices */
 	{ USB_DEVICE(0x0930, 0x021d), .driver_info = BTUSB_REALTEK },
